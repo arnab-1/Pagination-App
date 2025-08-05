35958d5a321d008e7e82cf0b40954836d3d69fe7
arnab-1 <arnab.vcc@gmail.com>
2025-08-05 20:26:50 +0530

    Done properly backend using mockdata and tested with Postman

---
+++ backend/data/mockData.js
@@ -0,0 +1,7 @@
+const items = [];
+
+for(let i = 1; i <= 50; i++) {
+    items.push({ id : i, name : `item ${i}`});
+}
+
+module.exports = items;
---
+++ backend/router/itemRoutes.js
@@ -0,0 +1,27 @@
+const express = require('express');
+const router = express.Router();
+const items = require('../data/mockData');
+
+router.get('/',(req,res) => {
+    console.log('hare');
+    
+    const page = parseInt(req.query.page) || 1;
+    const limit = parseInt(req.query.limit) || 10;
+
+    const start = (page - 1)* limit;
+    // const end = page* limit;
+    // I can compute end as = start + limit
+    const end = start + limit;
+    const paginatedItems = items.slice(start,end);
+
+    res.json({
+        totalItems : items.length,
+        totalPages : Math.ceil(items.length/limit),
+        currentPage : page,
+        items : paginatedItems
+    });
+
+
+});
+
+module.exports = router;
